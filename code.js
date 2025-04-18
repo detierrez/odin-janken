82a726d195fdfd4d467429e8ae32a6d4b1e13b49
detierrez <detierrez@gmail.com>
2025-04-17 20:46:16 -0400

    "Add feature to get computer and human choices"

--- code.js
+++ code.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+
+
+function getComputerChoice() {
+    const choice = randInt(3);
+    switch (choice) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+
+    }
+}
+
+function getHumanChoice() {
+    const choice = prompt(`Enter your choice:
+        0-Rock
+        1-Paper
+        2-Scissors`);
+    return parseInt(choice)
+}
+
+
+function randInt(highBound, lowBound = 0) {
+    return Math.floor(Math.random() * (highBound - lowBound)) + lowBound;
+}
