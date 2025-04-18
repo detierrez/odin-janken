8e4ab4f9d96bfaacb97844ea0aa3957c4ae39cc8
detierrez <detierrez@gmail.com>
2025-04-17 21:06:35 -0400

    Add game's single round feature

--- code.js
+++ code.js
@@ -1,26 +1,49 @@
+let humanScore = 0;
+let computerScore = 0;
 
+playGame();
+
+function playGame() {
+    for (let i = 1; i <= 5; i++) { 
+        console.log(`Round ${i}!`)
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return console.log(`Both chose ${humanChoice}! It's a tie!`)
+    }
+
+    const isWin = ((humanChoice === "rock") && (computerChoice === "scissors")) ||
+        ((humanChoice === "paper") && (computerChoice === "rock")) ||
+        ((humanChoice === "scissors") && (computerChoice === "paper"))
+    if (isWin) {
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}!`)
+    }
+
+    return console.log(`You lose! ${computerChoice} beats ${humanChoice}!`)
+}
+
+function getHumanChoice() {
+    const choice = prompt(`Enter your choice:
+        - Rock
+        - Paper
+        - Scissors`).toLowerCase();
+    return choice;
+}
 
 function getComputerChoice() {
     const choice = randInt(3);
     switch (choice) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
 
     }
 }
-
-function getHumanChoice() {
-    const choice = prompt(`Enter your choice:
-        0-Rock
-        1-Paper
-        2-Scissors`);
-    return parseInt(choice)
-}
-
 
 function randInt(highBound, lowBound = 0) {
     return Math.floor(Math.random() * (highBound - lowBound)) + lowBound;
